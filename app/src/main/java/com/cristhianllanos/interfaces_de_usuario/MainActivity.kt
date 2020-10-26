d689fdb1f82ccece1e90d97d8b99e79f84e65469
TristanCho <51831659+TristanCho@users.noreply.github.com>
2020-10-26 23:24:08 +0100

    Se plantea el salto entre distintos activities a través de Intent

--- app/src/main/java/com/cristhianllanos/interfaces_de_usuario/MainActivity.kt
+++ app/src/main/java/com/cristhianllanos/interfaces_de_usuario/MainActivity.kt
@@ -4,6 +4,7 @@ import android.content.Context
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
@@ -11,17 +12,23 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         btnCambiarSaludo.setOnClickListener{
-            val intent = Intent(this, MainActivity2::class.java)
+            tvSaludo.text ="Bienvenidos a Android, Programadores!!"
+            val  intent = Intent(this,MainActivity2::class.java)
             startActivity(intent)
         }
-        if(contexto == null)
+        if (contexto == null)
             contexto = this
     }
+
     override fun onResume() {
         super.onResume()
-        tvSaludo.text= "Son iguales los contextos: ${this ==contexto}"
+
+        tvSaludo.text = "Ahora el contexto es este !! Son iguales los contextos: ${this == contexto}"
+        Log.e("MainActivity", "Son iguales los contextos: ${this == contexto}")
     }
+
     companion object {
         private var contexto: Context? = null
     }
 }
+
--- app/src/main/java/com/cristhianllanos/interfaces_de_usuario/MainActivity2.kt
+++ app/src/main/java/com/cristhianllanos/interfaces_de_usuario/MainActivity2.kt
@@ -1,11 +1,23 @@
 package com.cristhianllanos.interfaces_de_usuario
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main2.*
 
 class MainActivity2 : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main2)
+        btnVolver.setOnClickListener{
+            onClick(it)
+        }
+    }
+    fun onClick(view:android.view.View){
+        val intent = Intent()
+        val datos ="Estos son datos generados en ${MainActivity2::class.simpleName}"
+        intent.putExtra("datos",datos)
+        setResult(RESULT_OK, intent)
+        finish()
     }
 }
